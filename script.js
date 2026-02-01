1088eb7dee0f103381ece990763237a5ffd50595
Zyrobots <zyrobots@outlook.com>
2026-02-01 15:01:11 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const items = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(e => {
+    if (e.isIntersecting) e.target.classList.add('active');
+  });
+}, { threshold: 0.2 });
+
+items.forEach(i => observer.observe(i));
