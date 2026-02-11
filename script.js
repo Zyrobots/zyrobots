0f5ac2c4c948639f68eaa2ba05343bdcc2f57887
Zyrobots <zyrobots@outlook.com>
2026-02-11 15:25:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -123,3 +123,18 @@ window.addEventListener('resize', () => {
   width = canvas.width = window.innerWidth;
   height = canvas.height = window.innerHeight;
 });
+
+/* ------------------------------
+   Cookie Banner
+--------------------------------*/
+const banner = document.getElementById("cookie-banner");
+const acceptBtn = document.getElementById("accept-cookies");
+
+if (localStorage.getItem("cookiesAccepted")) {
+  banner.style.display = "none";
+}
+
+acceptBtn.addEventListener("click", () => {
+  localStorage.setItem("cookiesAccepted", "true");
+  banner.style.display = "none";
+});
